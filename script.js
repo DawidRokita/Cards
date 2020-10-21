cb2589d9e87b2b3daebb133ae5e1ef297b97f426
Dawid <49307038+DawidRokita@users.noreply.github.com>
2020-10-21 11:29:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const icon = document.querySelector("#icon");
 const container = document.querySelector(".container");
 const photocircle = document.querySelector(".photocircle");
+const i = document.querySelectorAll(".i");
+const item9 = document.querySelector(".item9");
+const profile = document.querySelector(".profile");
 
 let x = false;
 
@@ -12,6 +15,14 @@ icon.addEventListener('click', function(){
     }
     
     x = !x;
-    container.style.background = x ? '#FFFFFF' : '#DFC63B';
+    // container.style.background = x ? '#FFFFFF' : '#DFC63B';
+
+    i.forEach(element => {
+        element.style.display = x ? "none" : "flex";
+    });
+
+        container.className = x ? "container2" : "container";
+        item9.style.display = x ? "flex" : "none";
+        profile.style.display = x ? "flex" : "none";
 
 })
