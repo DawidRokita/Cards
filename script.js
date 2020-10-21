918e1f3a05c4ef12e372b41b6e4e9ce6963d48c8
Dawid <49307038+DawidRokita@users.noreply.github.com>
2020-10-21 10:48:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const icon = document.querySelector("#icon");
 const container = document.querySelector(".container");
 const photocircle = document.querySelector(".photocircle");
 
+let x = false;
+
 icon.addEventListener('click', function(){
-    container.style.background = "white";
     if(icon.id === "icon"){
         icon.id = "icon2";
     }else{
         icon.id = "icon";
     }
     
+    x = !x;
+    container.style.background = x ? '#FFFFFF' : '#DFC63B';
+
 })
